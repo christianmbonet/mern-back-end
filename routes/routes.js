1b7512929dc738e8195a0c4badff554dd586357d
christianmbonet <christianm.bonet@gmail.com>
2021-05-17 09:51:10 -0400

    editing get post function

--- routes/routes.js
+++ routes/routes.js
@@ -48,14 +48,17 @@ router.get('/:id', (req, res) => {
 })
 
 const getPost = async (req, res, next) => {
+    let post
     try {
         post = await Post.findById(req.params.id)
         if (post == null) {
             return res.status(404).json({ message: 'cannot find post'})
         }
     } catch (err) {
-
+        return res.status(500).json({ message: err.message })
     }
+    res.post = post;
+    next()
 }
 
 module.exports = router;
